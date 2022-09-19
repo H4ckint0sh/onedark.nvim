9f987b180ce7e71bc415a1251d72fe352f306c92
Alijan Adeli <alijan.adeli@consid.se>
2022-09-19 22:20:40 +0200

    git blame

--- lua/onedark/theme.lua
+++ lua/onedark/theme.lua
@@ -377,6 +377,7 @@ theme.set_highlights = function(opts)
 
 	-- Git
 	hl(0, "gitcommitHeader", { fg = c.onedarkGray, bg = "NONE" })
+	hl(0, "GitNvimBlameHash", { fg = c.onedarkDisabled, bg = "NONE" })
 	hl(0, "gitcommitOnBranch", { fg = c.onedarkGray, bg = "NONE" })
 	hl(0, "gitcommitBranch", { fg = c.onedarkPink, bg = "NONE" })
 	hl(0, "gitcommitComment", { fg = c.onedarkGray, bg = "NONE" })
