8e7e2c9db4ad139421a1a618aa14129ef0f8b15d
Alijan Adeli <alijan.adeli@consid.se>
2022-09-21 21:13:43 +0200

    NvimTreeCursorline no bg

--- lua/onedark/theme.lua
+++ lua/onedark/theme.lua
@@ -503,7 +503,7 @@ theme.set_highlights = function(opts)
 	if isDark then
 		hl(0, "NvimTreeFolderIcon", { fg = c.onedarkBlue, bg = "NONE" })
 		hl(0, "NvimTreeIndentMarker", { fg = c.onedarkDisabled, bg = "NONE" })
-		hl(0, "NvimTreeCursorLine", { fg = "NONE", bg = c.onedarkDisabled })
+		hl(0, "NvimTreeCursorLine", { fg = "NONE", bg = "NONE" })
 		hl(0, "LspFloatWinNormal", { fg = c.onedarkFront, bg = "NONE" })
 		hl(0, "LspFloatWinBorder", { fg = c.onedarkFront, bg = "NONE" })
 		hl(0, "LspSagaHoverBorder", { fg = c.onedarkLineNumber, bg = "NONE" })
