50388482b745b9a1480cc03705a6e5047eece7c1
Alijan Adeli <alijan.adeli@consid.se>
2022-10-06 09:21:22 +0200

    pmenu changes

--- lua/onedark/init.lua
+++ lua/onedark/init.lua
@@ -2,49 +2,49 @@ local onedark = {}
 local theme = require("onedark.theme")
 
 onedark.setup = function(user_opts)
-    local defaults = {
-        transparent = false,
-        italic_comments = false,
-        color_overrides = {},
-        group_overrides = {},
-        disable_nvimtree_bg = true,
-    }
+	local defaults = {
+		transparent = false,
+		italic_comments = false,
+		color_overrides = {},
+		group_overrides = {},
+		disable_nvimtree_bg = true,
+	}
 
-    -- backwards compatibility: let users still set settings with global vars
-    local global_settings_opts = vim.tbl_extend("force", defaults, {
-        transparent = vim.g.onedark_transparency == 1,
-        italic_comments = vim.g.onedark_italic_comment == 1,
-        disable_nvimtree_bg = vim.g.onedark_disable_nvim_tree_bg == true,
-    })
+	-- backwards compatibility: let users still set settings with global vars
+	local global_settings_opts = vim.tbl_extend("force", defaults, {
+		transparent = vim.g.onedark_transparency == 1,
+		italic_comments = vim.g.onedark_italic_comment == 1,
+		disable_nvimtree_bg = vim.g.onedark_disable_nvim_tree_bg == true,
+	})
 
-    -- but override global vars settings with setup() settings
-    local opts = vim.tbl_extend("force", global_settings_opts, user_opts)
+	-- but override global vars settings with setup() settings
+	local opts = vim.tbl_extend("force", global_settings_opts, user_opts)
 
-    -- setting transparent to true removes the default background
-    if opts.transparent then
-        opts.color_overrides.vscBack = "NONE"
-    end
+	-- setting transparent to true removes the default background
+	if opts.transparent then
+		opts.color_overrides.vscBack = "NONE"
+	end
 
-    vim.cmd("hi clear")
-    if vim.fn.exists("syntax_on") then
-        vim.cmd("syntax reset")
-    end
+	vim.cmd("hi clear")
+	if vim.fn.exists("syntax_on") then
+		vim.cmd("syntax reset")
+	end
 
-    vim.o.termguicolors = true
-    vim.g.colors_name = "onedark"
+	vim.o.termguicolors = true
+	vim.g.colors_name = "onedark"
 
-    theme.set_highlights(opts)
-    theme.link_highlight()
+	theme.set_highlights(opts)
+	theme.link_highlight()
 
-    for group, val in pairs(opts["group_overrides"]) do
-        vim.api.nvim_set_hl(0, group, val)
-    end
+	for group, val in pairs(opts["group_overrides"]) do
+		vim.api.nvim_set_hl(0, group, val)
+	end
 end
 
 onedark.change_style = function(style)
-    vim.o.background = style
-    print("Vscode style: ", style)
-    vim.cmd([[colorscheme onedark]])
+	vim.o.background = style
+	print("Vscode style: ", style)
+	vim.cmd([[colorscheme onedark]])
 end
 
 return onedark
