b6bbda5b413522a7edc0864c74502df3d189e429
Alijan Adeli <alijan.adeli@consid.se>
2022-10-19 01:15:02 +0200

    search

--- lua/onedark/theme.lua
+++ lua/onedark/theme.lua
@@ -67,8 +67,8 @@ function M.setup()
 		PmenuThumb = { bg = c.Front }, -- Popup menu: Thumb of the scrollbar.
 		Question = { fg = c.Blue }, -- |hit-enter| prompt and yes/no questions
 		QuickFixLine = { bg = c.BackDark, bold = true }, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
-		Search = { bg = c.BackDark, fg = c.Front }, -- Last search pattern highlighting (see 'hlsearch').  Also used for similar items that need to stand out.
-		IncSearch = { bg = c.Yellow, fg = c.BackDark }, -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
+		Search = { bg = c.Search, fg = c.Front }, -- Last search pattern highlighting (see 'hlsearch').  Also used for similar items that need to stand out.
+		IncSearch = { bg = c.SearchCurrent, fg = c.Fron }, -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
 		CurSearch = { link = "IncSearch" },
 		SpecialKey = { fg = c.Comment }, -- Unprintable characters: text displayed differently from what it really is.  But not 'listchars' whitespace. |hl-Whitespace|
 		SpellBad = { sp = c.Error, undercurl = true }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
@@ -112,7 +112,7 @@ function M.setup()
 		-- Exception     = { }, --  try, catch, throw
 
 		PreProc = { fg = c.Purple, italic = true }, -- (preferRed) generic Preprocessor
-		Include = { fg = c.Yellow }, --  preprocessor #include
+		-- Include       = { }, --  preprocessor #include
 		-- Define        = { }, --   preprocessor #define
 		-- Macro         = { }, --    same as Define
 		-- PreCondit     = { }, --  preprocessor #if, #else, #endif, etc.
