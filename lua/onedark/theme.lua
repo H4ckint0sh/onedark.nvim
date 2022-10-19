2c6f418490d1a759a360d9db817de718811df96d
Alijan Adeli <alijan.adeli@consid.se>
2022-10-19 12:38:31 +0200

    tags to red and delimiters to comment

--- lua/onedark/theme.lua
+++ lua/onedark/theme.lua
@@ -246,8 +246,8 @@ function M.setup()
 		["@variable"]              = { fg = c.Front }, -- Any variable name that does not have another highlight.
 		["@variable.builtin"]      = { fg = c.Red }, -- Variable names that are defined by the languages, like `this` or `self`.
 
-		-- TSTag               = { };    -- Tags like html tag names.
-		-- TSTagDelimiter      = { };    -- Tag delimiter like `<` `>` `/`
+		["@tag"]            = { fg = c.Red }; -- Tags like html tag names.
+		["@tag.delimiter"]  = { fg = c.Comment }; -- Tag delimiter like `<` `>` `/`
 		-- TSText              = { };    -- For strings consideRed text in a markup language.
 		["@text.reference"] = { fg = c.Green },
 		-- TSEmphasis          = { };    -- For text to be represented with emphasis.
