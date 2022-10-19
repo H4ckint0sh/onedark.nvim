00f14acd8f3d26b5b6a74679612ee47f921a96ef
Alijan Adeli <alijan.adeli@consid.se>
2022-10-19 12:35:16 +0200

    variable to front

--- lua/onedark/theme.lua
+++ lua/onedark/theme.lua
@@ -243,7 +243,7 @@ function M.setup()
 		-- TSSymbol            = { };    -- For identifiers referring to symbols or atoms.
 		-- TSType              = { };    -- For types.
 		-- TSTypeBuiltin       = { };    -- For builtin types.
-		["@variable"]              = { fg = c.Red }, -- Any variable name that does not have another highlight.
+		["@variable"]              = { fg = c.Front }, -- Any variable name that does not have another highlight.
 		["@variable.builtin"]      = { fg = c.Red }, -- Variable names that are defined by the languages, like `this` or `self`.
 
 		-- TSTag               = { };    -- Tags like html tag names.
