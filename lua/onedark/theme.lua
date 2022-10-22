117db5cdab9a3b3abf71462c4ec447aedde9c63c
Alijan Adeli <alijan.adeli@consid.se>
2022-10-22 17:56:33 +0200

    adding bultin variable

--- lua/onedark/theme.lua
+++ lua/onedark/theme.lua
@@ -246,6 +246,7 @@ function M.setup()
 		-- TSType              = { };    -- For types.
 		-- TSTypeBuiltin       = { };    -- For builtin types.
 		["@variable"]              = { fg = c.Front }, -- Any variable name that does not have another highlight.
+		["@variable.builtin"]      = { fg = c.Red }, -- Any variable name that does not have another highlight.
 		["@tag"]                   = { fg = c.Red }; -- Tags like html tag names.
 		["@tag.attribute"]         = { fg = c.Orange };
 		["@tag.delimiter"]         = { fg = c.Comment }; -- Tag delimiter like `<` `>` `/`
